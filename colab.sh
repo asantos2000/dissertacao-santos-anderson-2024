fa94c9fcd28392f4c0a8c943b9d10947ff6d73d9
Anderson Santos <adsantos@gmail.com>
2024-10-29 12:20:24 -0300

    remove colab script

--- colab.sh
+++
@@ -1,6 +0,0 @@
-  rm -rf cfr2sbvr configuration checkpoint
-  git clone https://github.com/asantos2000/master-degree-santos-anderson.git cfr2sbvr
-  pip install -r cfr2sbvr/code/requirements.txt
-  cp -r cfr2sbvr/code/src/configuration .
-  cp -r cfr2sbvr/code/src/checkpoint .
-  cp -r cfr2sbvr/code/config.colab.yaml config.yaml
